cbe939abc4d4b2d96d8888bde6b9fea56ce2c5b4
daniel2dfla <daniel_2dfla@hotmail.com>
2025-03-19 17:25:48 -0300

    Feature: Configuring applications with PostgreSQL

---
+++ stateful/stateful-auth-api/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.user (id, username, password) VALUES (1, 'user.test', '$2a$10$L8ABo9n2xGkPQITFB81uzu8Wz1rK9Y67GlVSQGsk78AY22nGMmdhG')
+INSERT INTO public.user (id, username, password) VALUES (2, 'user.new.test', '$2a$10$L8ABo9n2xGkPQITFB81uzu8Wz1rK9Y67GlVSQGsk78AY22nGMmdhG')
